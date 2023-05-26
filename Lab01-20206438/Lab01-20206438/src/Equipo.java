b11e801276c2d5150eaa874b89cc750e78057d3d
Sebastian <a20206438@pucp.edu.pe>
2023-05-25 19:55:04 -0500

    listo

---
+++ Lab01-20206438/Lab01-20206438/src/Equipo.java
@@ -0,0 +1,56 @@
+public class Equipo {
+    private String pais;
+    private String nombre;
+    private String iniciales;
+    private String estadio;
+    private String tecnico;
+    private float presupuesto;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIniciales() {
+        return iniciales;
+    }
+
+    public void setIniciales(String iniciales) {
+        this.iniciales = iniciales;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public float getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(float presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+}
